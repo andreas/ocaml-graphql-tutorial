e4e18ba3b2355bd7293e24d6f7761196715bd523
Andreas Garnaes <andreas.garnaes@gmail.com>
2017-09-03 13:08:14 +0200

    Solutions to exercise 5.

--- server.ml
+++ server.ml
@@ -1,5 +1,21 @@
 open Graphql_lwt
 
+module List = struct
+  include List
+
+  let rec drop t ~n =
+    match t with
+    | [] -> t
+    | _ when n <= 0 -> t
+    | _::xs -> drop xs ~n:(n-1)
+
+  let rec take ?(memo=[]) t ~n =
+    match t with
+    | [] -> List.rev memo
+    | _ when n <= 0 -> List.rev memo
+    | x::xs -> take ~memo:(x::memo) xs ~n:(n-1)
+end
+
 let packages = Package.load_all ()
 
 let package = Schema.(obj "Package"
@@ -26,10 +42,25 @@ let schema = Schema.(schema [
     ;
     field "packages"
       ~typ:(non_null (list (non_null package)))
-      ~args:Arg.[]
-      ~resolve:(fun _ () ->
-        packages
+      ~args:Arg.[
+        arg' "page" ~typ:int ~default:1;
+        arg' "per_page" ~typ:int ~default:10;
+      ]
+      ~resolve:(fun _ () page per_page ->
+        let offset = (page-1)*per_page in
+        List.drop ~n:offset packages
+        |> List.take ~n:per_page
       )
+    ;
+    field "package"
+      ~typ:package
+      ~args:Arg.[
+        arg ~typ:(non_null string) "name"
+      ]
+      ~resolve:(fun _ () name ->
+        Package.find_by_name packages name
+      )
+    ;
   ]
 )
 
