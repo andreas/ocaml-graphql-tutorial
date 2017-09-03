67d21ec777346f230d87189ff35045418b4dc3e5
Andreas Garnaes <andreas.garnaes@gmail.com>
2017-09-03 12:49:11 +0200

    Solutions to exercise 4.

--- server.ml
+++ server.ml
@@ -2,8 +2,34 @@ open Graphql_lwt
 
 let packages = Package.load_all ()
 
+let package = Schema.(obj "Package"
+  ~fields:(fun _ -> [
+    field "name"
+      ~typ:(non_null string)
+      ~args:Arg.[]
+      ~resolve:(fun _ pkg -> pkg.Package.name)
+    ;
+    field "version"
+      ~typ:(non_null string)
+      ~args:Arg.[]
+      ~resolve:(fun _ pkg -> pkg.Package.version)
+  ])
+)
+
 let schema = Schema.(schema [
-    (* Add fields here *)
+    field "packageCount"
+      ~typ:(non_null int)
+      ~args:Arg.[]
+      ~resolve:(fun _ () ->
+        List.length packages
+      )
+    ;
+    field "packages"
+      ~typ:(non_null (list (non_null package)))
+      ~args:Arg.[]
+      ~resolve:(fun _ () ->
+        packages
+      )
   ]
 )
 
